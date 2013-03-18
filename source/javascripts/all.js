514e557f6000faccaf03d6a51eb3c88d57cf7cf1
Tyler Mauthe <me@tylermauthe.com>
2013-03-18 07:29:20 -0600

    Add DailyCred

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,18 @@
 //= require "bootstrap"
+
+//DailyCred
+(function() {
+  var dc, dc_opts, url;
+  dc_opts = {
+    clientId: "92385cdc-829f-4dc7-82f0-9212098fa53c",
+    home: "https://www.dailycred.com",
+    personaAudience: ""
+  };
+  dc = document.createElement("script");
+  url = dc_opts.home + "/public/js/cred.coffee";
+  dc.src = url;
+  $(document).ready(function (){
+  	document.body.appendChild(dc);
+  });
+  window.dc_opts = dc_opts;
+}).call(this);
