44a13081542473dc3c48ceb204008d2042da2ffc
Tyler Mauthe <me@tylermauthe.com>
2013-03-19 20:34:21 -0600

    Updated landing page a bit.

--- source/javascripts/landing.js
+++ source/javascripts/landing.js
@@ -1,5 +1,9 @@
 (function ($) {
 	$(document).ready(function() {
-
+		window.setTimeout(function() {
+			if($(document).scrollTop()==0) {
+				//implement code here to entice user to scroll past fold
+			}
+		}, 5000);
 	});
 })(window.jQuery)
