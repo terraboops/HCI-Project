862daf9c0eefc65428b36b00f21c0192a678fa58
Tyler Mauthe <me@tylermauthe.com>
2013-03-24 18:02:14 -0600

    Replaced jQuery animation with Vanilla JS

--- source/javascripts/photostream.js
+++ source/javascripts/photostream.js
@@ -1,43 +1,26 @@
 (function ($) {
-	// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-	// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-	 
-	// requestAnimationFrame polyfill by Erik Möller
-	// fixes from Paul Irish and Tino Zijdel
-	 
-	(function() {
-	    var lastTime = 0;
-	    var vendors = ['ms', 'moz', 'webkit', 'o'];
-	    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-	        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-	        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-	                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-	    }
-	 
-	    if (!window.requestAnimationFrame)
-	        window.requestAnimationFrame = function(callback, element) {
-	            var currTime = new Date().getTime();
-	            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-	            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-	              timeToCall);
-	            lastTime = currTime + timeToCall;
-	            return id;
-	        };
-	 
-	    if (!window.cancelAnimationFrame)
-	        window.cancelAnimationFrame = function(id) {
-	            clearTimeout(id);
-	        };
-	}());
 
 	$(document).ready(function (){
-		requestAnimationFrame(animationLoop);
+		window.setInterval(animationLoop, 100, 1);
 	});
-	function animationLoop(){
-		$('#photostream').animate({backgroundPositionX: '-1000px'}, 140000, 'linear',function (){
-			$('#photostream').animate({backgroundPositionX: '1000px'}, 140000, 'linear', function(){
-				requestAnimationFrame(animationLoop);
-			});
-		});
+	function animationLoop(deltaX, deltaY){
+		if (typeof deltaX === "undefined")
+		{
+			deltaX = 0;
+		}
+		if (typeof deltaY === "undefined")
+		{
+			deltaY = 0;
+		}
+		var element = document.getElementById('photostream');
+		var position = window.getComputedStyle(element).getPropertyValue('background-position').split(' ');
+		var pattern = /[0-9]+/;
+		var x = window.parseInt(pattern.exec(position[0]),10);
+		var y = window.parseInt(pattern.exec(position[1]),10);
+		var xUnit = position[0].split(pattern)[1];
+		var yUnit = position[1].split(pattern)[1];
+		x = x + deltaX;
+		y = y + deltaY;
+		element.style.backgroundPosition =x+xUnit+" "+y+yUnit;
 	};
 })(window.jQuery);
