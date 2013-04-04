625f2eba7aabcc13eea2025ddd72f9c1ab125d55
Tyler Mauthe <me@tylermauthe.com>
2013-04-04 09:50:50 -0600

    Updated Photostream

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ source 'http://rubygems.org'
 gem "middleman", "~>3.0.12"
 gem "middleman-livereload", "~>3.0.1"
 gem "oj", "~>2.0.10"
+gem "rb-inotify", '~>0.9'
--- source/javascripts/photostream.js
+++ source/javascripts/photostream.js
@@ -1,24 +1,41 @@
 (function () {
-	animationLoop(0.01);
-	window.setInterval(animationLoop, 10, 0.01);
-	function animationLoop(deltaX, deltaY){
-		if (typeof deltaX === "undefined")
-		{
-			deltaX = 0;
+	var animationLoop = animationFactory(0.01,0)
+	animationLoop();
+	window.setInterval(animationLoop, 10);
+	function animationFactory(deltaX, deltaY) {
+		return function () {
+			(function (deltaX, deltaY) {
+				if (typeof deltaX === "undefined") {
+					deltaX = 0;
+				}
+				if (typeof deltaY === "undefined") {
+					deltaY = 0;
+				}
+				var element = document.getElementById('photostream');
+				var position = window.getComputedStyle(element).getPropertyValue('background-position').split(' ');
+				
+				var pattern = /[0-9\.]+/;
+				
+				var x = window.parseFloat(pattern.exec(position[0]));
+				var y = window.parseFloat(pattern.exec(position[1]));
+				x = x + deltaX;
+				y = y + deltaY;
+				
+				if(x===0) {
+					var xUnit = "";
+				}
+				else {
+					var xUnit = position[0].split(pattern)[1];
+				}
+				if(y===0) {
+					var yUnit = "";
+				}
+				else {
+					var yUnit = position[1].split(pattern)[1];
+				}
+				
+				element.style.backgroundPosition = x + xUnit + " " + y + yUnit;
+			})(deltaX, deltaY);
 		}
-		if (typeof deltaY === "undefined")
-		{
-			deltaY = 0;
-		}
-		var element = document.getElementById('photostream');
-		var position = window.getComputedStyle(element).getPropertyValue('background-position').split(' ');
-		var pattern = /[0-9\.]+/;
-		var x = window.parseFloat(pattern.exec(position[0]));
-		var y = window.parseFloat(pattern.exec(position[1]));
-		var xUnit = position[0].split(pattern)[1];
-		var yUnit = position[1].split(pattern)[1];
-		x = x + deltaX;
-		y = y + deltaY;
-		element.style.backgroundPosition =x+xUnit+" "+y+yUnit;
-	};
+	}
 })();
